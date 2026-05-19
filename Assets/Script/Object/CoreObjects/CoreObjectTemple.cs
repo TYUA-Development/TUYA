4d28ccfc56ed21fc576130b93afdc53d9c0374ea
backsani <118903562+backsani@users.noreply.github.com>
2026-05-20 01:02:10 +0900

    Temple이 올라갈 때 플레이어도 같이 올라가는 기능 구현

--- Assets/Script/Object/CoreObjects/CoreObjectTemple.cs
+++ Assets/Script/Object/CoreObjects/CoreObjectTemple.cs
@@ -21,13 +21,26 @@ public class CoreObjectTemple : MonoBehaviour, IArrowHit, ICoreEvent
     public List<TemplePiece> pieces;
     public bool activateTemple;
 
+    public int includePlayerIndex = -1;
+    public PlayerController controller;
+
     public void OnCoreEvent()
     {
         activateTemple = true;
 
+        int index = 0;
+
         foreach (TemplePiece piece in pieces)
-        {
-            StartCoroutine(RisingTemple(piece));
+        { 
+            if(index++ == includePlayerIndex)
+            {
+                StartCoroutine(RisingTempleAndPlayer(piece));
+
+            }
+            else
+            {
+                StartCoroutine(RisingTemple(piece));
+            }
         }
     }
 
@@ -94,5 +107,66 @@ public class CoreObjectTemple : MonoBehaviour, IArrowHit, ICoreEvent
 
         // 정확한 위치 보정
         target.localPosition = targetPos;
+    }
+
+    private IEnumerator RisingTempleAndPlayer(TemplePiece piece)
+    {
+        float originGravity = controller.Rigidbody2D.gravityScale;
+        controller.Rigidbody2D.gravityScale = 0;
+
+        yield return new WaitForSeconds(piece.delayTime);
+
+        Transform target = piece.piece.transform;
+
+        // 시작 위치
+        Vector3 startPos = target.localPosition;
+
+        Vector3 prevWorldPos = target.position;
+
+        // 목표 위치 (Y만 변경)
+        Vector3 targetPos = new Vector3(
+            startPos.x,
+            piece.targetPosY,
+            startPos.z);
+
+        float elapsed = 0f;
+
+        while (elapsed < piece.time)
+        {
+            elapsed += Time.deltaTime;
+
+            // 진행도 (0 ~ 1)
+            float t = Mathf.Clamp01(elapsed / piece.time);
+
+            // 감속 강도 계산
+            float power = Mathf.Lerp(1.5f, 8f, piece.slowPower / 100f);
+
+            // Ease-Out 곡선
+            float curvedT = 1f - Mathf.Pow(1f - t, power);
+
+            // localPosition 기준 이동
+            target.localPosition = Vector3.Lerp(
+                startPos,
+                targetPos + piece.noise.LerpNoise(),
+                curvedT);
+
+            // 현재 월드 위치
+            Vector3 currentWorldPos = target.position;
+
+            // 이동량 계산
+            Vector3 delta = currentWorldPos - prevWorldPos;
+
+            // 플레이어 같이 이동
+            controller.transform.position += delta;
+
+            // 현재 위치 저장
+            prevWorldPos = currentWorldPos;
+
+            yield return null;
+        }
+
+        // 정확한 위치 보정
+        target.localPosition = targetPos;
+        controller.Rigidbody2D.gravityScale = originGravity;
     }
 }
